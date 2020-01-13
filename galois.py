23c83461f8ac2e353db20da2029056aa46f464a8
kevinsantana <kevin_santana.araujo@hotmail.com>
2020-01-12 21:52:11 -0300

    Alteração da estrutura de dados do corpo de Galois

--- galois.py
+++ galois.py
@@ -1,48 +1,54 @@
-class galois():
+from collections import OrderedDict
+
+class Galois():
     def __init__(self, primitivo: int, tamanho: int) -> list:
         self.primitivo = primitivo
         self.tamanho = tamanho
-        self.decimais = list()        
+        self.corpo_galois = OrderedDict()        
         self.inicializar_galois()
 
 
     def inicializar_galois(self):
-        # indices: alfa; valores: decimais        
+        # chave: alfa; valores: forma decimal        
         x = 1        
-        for _ in range(self.tamanho):
-            self.decimais.append(x)
+        self.corpo_galois["zero"] = 0
+        for indice in range(self.tamanho - 1):            
+            self.corpo_galois[indice] = x
             x = x * 2
             if x >= self.tamanho:
-                x ^= self.primitivo
-        self.inicializado = True    
+               x ^= self.primitivo        
 
                 
     def soma_subtracao(self, a: int, b: int) -> int:
-        # a ou b sao decimais
+        '''
+        Implementa a soma e subtração entre dois elementos de um corpo de Galois, que estejam no mesmo GF(tamanho)        
+        '''
         return a ^ b
 
 
     def multiplicacao(self, a: int, b: int) -> int:
-        # a ou b sao decimais
-        # corrigir para suportar multiplicacao entre coeficientes de um polinomio
+        # a ou b sao decimais em um gf        
         if a == 0 or b == 0:
             return 0 
-        return self.decimais[(self.decimais.index(a) + self.decimais.index(b)) % (self.tamanho - 1)]
+        return self.corpo_galois[(self.grau(a) + self.grau(b)) % (self.tamanho - 1)]
 
     
     def elemento_inverso(self, a: int) -> int:
         if a == 0:
             raise ArithmeticError()        
-        return self.decimais[(- self.decimais.index(a) % (self.tamanho - 1))]
-
-
-    # def divisao(self, a: int, b: int) -> int:
-    #     # a ou b sao decimais
-    #     return self.decimais[(self.decimais.index(a) - self.decimais.index(b)) % (self.tamanho - 1)]
+        return self.corpo_galois[(- self.grau(a) % (self.tamanho - 1))]
 
     
     def divisao(self, a: int, b: int) -> int:
-        # a ou b sao decimais
-        # a / b
+        # a ou b sao decimais em um gf # a / b      
         return self.multiplicacao(a, self.elemento_inverso(b))
 
+
+    def grau(self, elemento: int):
+        return [chave for chave, valor in self.corpo_galois.items() if elemento == valor].pop()
+
+
+if __name__ == "__main__":
+    gf_16 = Galois(19,16)        
+    print(gf_16.divisao(11,10))
+    print(gf_16.corpo_galois)
--- polinomio_galois.py
+++ polinomio_galois.py
@@ -1,21 +1,24 @@
 from galois import *
 
-class polinomio_galois():
+
+class PolinomioGalois(Galois):
     # coeficientes: do maior para o menor grau
-    def __init__(self, corpo: galois, coeficientes: list):
+    def __init__(self, corpo: Galois, coeficientes: dict):
         if len(coeficientes) == 0:
             raise ArithmeticError()
         self.corpo = corpo
         self.coeficientes = coeficientes
 
     
-    def multiplicacao_polinomios(self, polinomio) -> list:
-        return [galois.multiplicacao(self.corpo,a,b) for a in self.coeficientes for b in polinomio]
+    def multiplicacao_polinomios_gf(self, polinomio) -> dict:
+        # comparar e lançar exceção caso o corpo de um polinomio seja diferente do outro
+        return {Galois.multiplicacao(self.corpo,a,b) for a in self.coeficientes.values() for b in polinomio.values()}
 
 
-gf_16 = galois(19,16)        
-coeficientes = [1,2,3,4,5,6,7,8,9,10,11]
-polinomio = polinomio_galois(gf_16, coeficientes)
-coeficientes2 = [3]
-multiplicaco = polinomio.multiplicacao_polinomios(coeficientes2)
-print(gf_16.decimais.index(3))
+gf_16 = Galois(19,16)        
+polinomio = {10: 1, 9: 2, 8: 3, 7: 4, 6: 5, 5: 6, 4: 7, 3: 8, 2: 9, 1: 10, 0: 11}
+polinomio2 = {4: 1}
+pgf1 = PolinomioGalois(gf_16, polinomio)
+pgf1 = PolinomioGalois(gf_16, polinomio2)
+multiplicaco = pgf1.multiplicacao_polinomios_gf(polinomio2)
+print(multiplicaco)
